2eeba75314d39ccac757dba580bb64a8d275e2b8
Nathanael Scudder <nscudder@uci.edu>
2022-12-09 18:16:06 -0800

    Finances Page Refactor Commit

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -12,8 +12,8 @@ const routes: Routes = [
         loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'finances',
+        loadChildren: () => import('../finances/finances.module').then(m => m.FinancesPageModule)
       },
       {
         path: 'tab3',
